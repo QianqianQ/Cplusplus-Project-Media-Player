ce5b9045548bd206cd5f4bfb9639e78189378843
黎晓浦 <lixiaopu@lixiaopudeMacBook-Pro.local>
2017-12-03 03:59:37 +0200

    playlist.h

---
+++ src/qt-project/playlist.h
@@ -0,0 +1,28 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
+#include "QStringList"
+#include "vector"
+#include "trackfile.h"
+
+
+class Playlist
+{
+public:
+    Playlist();
+
+    void add(QStringList files);
+
+    void remove(int index);
+
+    void save();
+
+    QStringList getFileList();
+
+    std::vector<Trackfile> tracks;
+
+private:
+
+};
+
+#endif // PLAYLIST_H
